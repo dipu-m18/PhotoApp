355e2e07713728dc4c6f2e9dd0266fd8696e58c1
dipu-m18 <dipannitamahata@gmail.com>
2022-04-08 17:10:50 +0530

    Created custom filter for authentication

--- ApiGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/appsdeveloperblog/photoapp/api/gateway/ApiGatewayApplication.java
@@ -1,10 +1,7 @@
 package com.appsdeveloperblog.photoapp.api.gateway;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
